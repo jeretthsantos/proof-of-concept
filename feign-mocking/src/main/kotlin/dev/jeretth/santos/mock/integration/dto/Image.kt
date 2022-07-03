5a87b0e7c52f6ef31b514f02adb29c82f3825dc0
Jeretth Santos <jeretthrmsantos@protonmail.com>
2022-07-03 11:23:28 +0800

    Remove feign mocking project

--- feign-mocking/src/main/kotlin/dev/jeretth/santos/mock/integration/dto/Image.kt
+++
@@ -1,9 +0,0 @@
-package dev.jeretth.santos.mock.integration.dto
-
-data class Image(
-    val url: String,
-    val artist: String,
-    val artistUrl: String?,
-    val sourceUrl: String?,
-    val error: String
-)
